86e466b294bc6fcf209df8bc02190b2b7c4a17e1
liam <liamk@mun.ca>
2023-11-15 00:59:43 -0330

    add styles

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,9 @@ export default function About() {
     return (
         <section id="about" className="section">
             <div className="about-me p-10 text-lg mt-10 text-grn-300 justify-between sm:w-full md:w-full lg:w-5/6">
-
-                <p>My interest with technology and software began during childhood with the introduction to my familys Pentium III PC.
+                <p>My interest with technology and software began during childhood with the introduction to my familys Pentium 4 PC in the early 2000's.
                 </p>
-                <p className="mt-3">                    As I got older, I wanted to challenge myself by digging deeper into the tools and technologies I'd been using for years.
+                <p className="mt-3">As I got older, I wanted to challenge myself by digging deeper into the tools and technologies I'd been using for years.
                     This led me to pursue a degree in Computer Science, refining my skills and understanding of software engineering and design.</p>
             </div>
         </section>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import RightPanel from "../components/RightPanel"
 
 export default function Home() {
     return (
-        <div className="container mx-auto my-auto">
+        <div className="container mx-auto my-auto selection:bg-gray-200 selection:text-grn-700">
             <div className="grid md:justify-items-start mx-auto lg:ml-20"><LeftPanel/></div>
             <div className="grid md:justify-items-end mx-auto"><RightPanel/></div>
         </div>
